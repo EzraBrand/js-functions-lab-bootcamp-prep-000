adaa439fc892de712df4dd4a8ea6fd28d21f49d6
Ezra Brand <ezrabrand@gmail.com>
2019-04-02 14:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays() {
+  console.log('Happy holidays!')
+}
+
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+
+function happyHolidaysToYou(holiday, name) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+
+
+function holidayCountdown(days, holiday) {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
